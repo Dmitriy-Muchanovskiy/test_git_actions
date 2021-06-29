40a379f26e21ab6ed5489202cb627c61800fe430
Dmitry Muchanovskiy <mitrichm2@gmail.com>
2021-06-29 19:49:52 +0300

    go unit-test 0.4 cover with sh

--- go_test/cover.sh
+++ go_test/cover.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-cover=$(go test -cover | grep -o " .*%" | cut -c 2,3)
+cover=$(go test -coverprofile=cover.txt | grep -o " .*%" | cut -c 2,3)
 if [ $cover -gt 0 ]
 then 
     echo "coverage: $cover% of statements"
