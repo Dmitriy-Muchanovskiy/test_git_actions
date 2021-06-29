2fd235deb188f02a1263e90242bcf482faa13e55
Dmitry Muchanovskiy <mitrichm2@gmail.com>
2021-06-29 19:42:32 +0300

    go unit-test 0.3 cover with sh

--- go_test/cover.sh
+++ go_test/cover.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
 cover=$(go test -cover | grep -o " .*%" | cut -c 2,3)
-if [ $cover -lt 50 ]; then exit 1;fi
+if [ $cover -gt 0 ]
+then 
+    echo "coverage: $cover% of statements"
+else 
+    echo "coverage 0% exit job" 
+    exit 1
+fi
